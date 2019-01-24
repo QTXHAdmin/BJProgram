c124142946703e9fc516d759331498c7f5fc2036
zhanghe888 <15942892352@sina.cn>
2019-01-24 09:15:44 +0800

    added second zh

--- src/views/zhangheShopCar/OrderInsideActive.vue
+++ src/views/zhangheShopCar/OrderInsideActive.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="orderinsideactive">
-
     <PublicTopHeader :title="orderinsidetitle">
       <span slot="top-r" class="end-visit">结束拜访</span>
     </PublicTopHeader>
-
+    <div class="shop-wrap">
+      <div class="shopname"></div>
+      <div class="shop-sales">
+        <div class="shop-sales-title">本月至今销量</div>
+        <div class="count">23198</div>
+      </div>
+      <div class="average-sales">
+        <div class="average-sales-title">过往半年月均销量</div>
+        <div class="count">56660</div>
+      </div>
+      <div class="percent">
+        <div class="percent-title">核心分销完成率</div>
+        <div class="count">68%</div>
+      </div>
+      <div class="history"></div>
+      <textarea name="" id=""></textarea>
+    </div>
   </div>
 </template>
 <script>
@@ -22,7 +37,26 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.end-visit {
-  font-size: px2rem(20);
+.orderinsideactive {
+  .end-visit {
+    font-size: px2rem(25);
+  }
+  .shop-wrap {
+    .count {
+    }
+    .shopname {
+      height: px2rem(84);
+      background-color: #fafafa;
+      .shop-sales-title {
+      }
+    }
+    .shop-sales {
+      height: px2rem(110);
+    }
+    .average-sales {
+    }
+    .percent {
+    }
+  }
 }
 </style>
